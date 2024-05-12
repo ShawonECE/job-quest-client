3ff0dc8f567ae3d2a29941f7a3ab19861755ed22
ShawonECE <send2shawon@gmail.com>
2024-05-12 14:55:18 +0600

    delete functionality implemented in myjobs implemented

--- src/components/MyJobs.jsx
+++ src/components/MyJobs.jsx
@@ -1,7 +1,67 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "./AuthProvider";
+import axios from "axios";
+import { Helmet } from "react-helmet-async";
+import MyJobsTableRow from "./MyJobsTableRow";
+import swal from "sweetalert";
+
 const MyJobs = () => {
+    const { user } = useContext(AuthContext);
+    const [myJobs, setMyJobs] = useState([]);
+    useEffect(() => {
+        axios.get(`http://localhost:3000/jobs?email=${user.email}`)
+        .then(data => setMyJobs(data.data));
+    }, [user]);
+    const handleDelete = (id) => {
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this data!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+        .then((willDelete) => {
+            if (willDelete) {
+                axios.delete(`http://localhost:3000/${id}`)
+                .then(data => {
+                    if (data.data.deletedCount === 1) {
+                        swal("Poof! Data has been deleted!", {
+                            icon: "success",
+                        });
+                        const remaining = myJobs.filter(job => job._id !== id);
+                        setMyJobs(remaining);
+                    } else {
+                        swal("Deletion failed!", {
+                            icon: "warning",
+                        });
+                    }
+                });
+            }
+        });
+    };
     return (
-        <div>
-            Here is your jobs
+        <div className="mt-8">
+            <Helmet>
+                <title>JobQuest | Your Jobs</title>
+            </Helmet>
+            <h1 className="text-center text-3xl font-bold">You have added {myJobs.length} jobs</h1>
+            <div className="overflow-x-auto bg-[#E7F6F2] rounded-xl mt-4">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Job Title</th>
+                            <th>Posting Date</th>
+                            <th>Application Deadline</th>
+                            <th>No. of Applicants</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myJobs.map(job => <MyJobsTableRow key={job._id} handleDelete={handleDelete} job={job}></MyJobsTableRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/components/MyJobsTableRow.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { MdDeleteForever } from "react-icons/md";
+
+const MyJobsTableRow = ({job, handleDelete}) => {
+    const {job_title, job_posting_date, deadline, number_of_applicants, _id} = job;
+    return (
+        <tr>
+            <td>{job_title}</td>
+            <td>{job_posting_date}</td>
+            <td>{deadline}</td>
+            <td>{number_of_applicants}</td>
+            <td><button className="btn btn-xs bg-[#2C3333] text-[#E7F6F2]">Update</button></td>
+            <td><MdDeleteForever onClick={() => handleDelete(_id)} className='text-red-600 text-2xl cursor-pointer' /></td>
+        </tr>
+    );
+};
+
+MyJobsTableRow.propTypes = {
+    job: PropTypes.object.isRequired,
+    handleDelete: PropTypes.func.isRequired
+};
+
+export default MyJobsTableRow;
