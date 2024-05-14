393e30ac318cf966cadd752c1a7398acd6ce8ad4
ShawonECE <send2shawon@gmail.com>
2024-05-14 09:06:25 +0600

    framer-motion transition implemented on job-card

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,11 +1,21 @@
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
 
 const JobCard = ({job}) => {
     const {_id, posted_by, job_title, job_posting_date, deadline, salary_range, number_of_applicants, job_img, job_category} = job;
     const navigate = useNavigate();
+    const ref = useRef(null)
+    const inView = useInView(ref);
     return (
-        <div className="card card-compact bg-[#E7F6F2] dark:bg-[#2C3333] dark:text-[#E7F6F2] shadow-xl">
+        <motion.div
+            ref={ref}
+            className="card card-compact bg-[#E7F6F2] dark:bg-[#2C3333] dark:text-[#E7F6F2] shadow-xl"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 20 }}
+            transition={{ duration: 0.5 }}
+        >
             <figure><img src={job_img} alt="job" /></figure>
             <div className="card-body">
                 <h2 className="card-title">
@@ -23,7 +33,7 @@ const JobCard = ({job}) => {
                     <button onClick={() => navigate(`/job/${_id}`)} className="btn bg-[#2C3333] text-[#E7F6F2]">View Details</button>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 };
 
